20f125b8e70f7c32173b120a546b8e4daba78871
manrajrrs <manrajrrsingh@gmail.com>
2025-03-16 07:21:58 +0000

    Updated rock-paper-scissors with improved gameplay

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,4 +2,21 @@ import random
 
 choices = ["rock", "paper", "scissors"]
 computer_choice = random.choice(choices)
-print(computer_choice)
+
+player_choice = input("Please enter rock, paper or scissors:").lower()
+
+while player_choice not in choices:
+    player_choice = input(f"{player_choice} is not a valid input. Please enter rock, paper or scissors:").lower()
+
+print(f"Computer chose {computer_choice}!")
+    
+if player_choice == computer_choice:
+    print("It's a draw!")
+    
+elif    (player_choice == "rock" and computer_choice == "scissors") or \
+        (player_choice == "paper" and computer_choice == "rock") or \
+        (player_choice == "scissors" and computer_choice == "paper"):
+    print("You win!")
+    
+else:
+    print("You lose...")
